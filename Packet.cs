3c3eff342e96938bd9188775abc7c6255fd83c2e
junhee <hunroung02@naver.com>
2023-05-30 07:33:26 +0900

    server

---
+++ Packet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace TextRpg_packet
+{
+    public enum Packet_Type
+    {
+        보스=0,
+        챕터,
+        패턴
+        
+    }
+    [Serializable]
+    public class Packet
+    {
+        public int Length;
+        public int Typee;
+
+        public Packet()
+        {
+            this.Length = 0;
+            this.Typee = 0;
+        }
+        sealed class PreMergeToMergedDeserializationBinder : SerializationBinder
+        {
+            public override Type BindToType(string assemblyName, string typeName)
+            {
+                Type typeToDeserialize = null;
+
+                // For each assemblyName/typeName that you want to deserialize to
+                // a different type, set typeToDeserialize to the desired type.
+                String exeAssembly = Assembly.GetExecutingAssembly().FullName;
+
+
+                // The following line of code returns the type.
+                typeToDeserialize = Type.GetType(String.Format("{0}, {1}",
+                    typeName, exeAssembly));
+
+                return typeToDeserialize;
+            }
+        }
+        public static byte[] Serialize(Object o)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+        public static Object Desserialize(byte[] bt)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            ms.Write(bt, 0, bt.Length);
+            ms.Position = 0;
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Binder = new PreMergeToMergedDeserializationBinder();
+            Object obj = bf.Deserialize(ms);
+            ms.Close();
+            return obj;
+        }
+    }
+    [Serializable]
+    public class Boss : Packet
+    {
+        public int value = 0;
+    }
+    [Serializable]
+    public class Pattern : Packet
+    {
+        public int chapter = 0;
+        public int pattern = 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,151 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using TextRpg_packet;
+
+namespace Server
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.ServerStart();
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+    }
+    class Server
+    {
+        const int PORT = 8066;
+        private TcpListener m_listener;
+        public void ServerStart()
+        {
+            try
+            {
+                m_listener = new TcpListener(PORT);
+                m_listener.Start();
+
+                while(true)
+                {
+                    TcpClient hClient = m_listener.AcceptTcpClient();
+
+                    if(hClient.Connected)
+                    {
+                        Handler handle = new Handler();
+                        Thread client=new Thread(handle.handling);
+                        client.Start(hClient);
+                    }
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Server Error");
+            }
+        }
+    }
+    class Handler
+    {
+        public NetworkStream m_Stream;
+        public bool m_bStop = false;
+
+        private byte[] sendBuffer=new byte[1024*4];
+        private byte[] readBuffer=new byte[1024*4];
+
+        TcpClient client_socket;
+
+        public Boss m_boss;
+        public Pattern m_pattern;
+
+        public void handling(object client_socket)
+        {
+            this.client_socket = (TcpClient)client_socket;
+            m_Stream=this.client_socket.GetStream();
+            m_bStop = true;
+            int n_read = 0;
+
+            while(m_bStop)
+            {
+                try
+                {
+                    n_read = 0;
+                    n_read=this.m_Stream.Read(readBuffer,0,1024*4);
+                }
+                catch
+                {
+                    this.m_bStop = false;
+                    this.m_Stream= null;
+                    return;
+                }
+
+                Packet packet = (Packet)Packet.Desserialize(this.readBuffer);
+
+                switch ((int)packet.Typee)
+                {
+                    case (int)Packet_Type.보스:
+                        this.m_boss=(Boss)Packet.Desserialize(this.readBuffer);
+                        m_boss.value = BossOut(m_boss.value);
+                        Packet.Serialize(m_boss).CopyTo(this.sendBuffer,0);
+                        Send();
+                        break;
+                    case (int)Packet_Type.패턴:
+                        this.m_pattern=(Pattern)Packet.Desserialize(this.readBuffer);
+                        m_pattern.pattern = PatternOut(m_pattern.chapter,m_pattern.pattern);
+                        Packet.Serialize(m_pattern).CopyTo(this.sendBuffer,0);
+                        Send();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Send()
+        {
+            this.m_Stream.Write(this.sendBuffer,0,this.sendBuffer.Length);
+            this.m_Stream.Flush();
+            for (int i=0;i<1024*4;i++)
+            {
+                this.sendBuffer[i]=0;
+            }
+        }
+
+        private int PatternOut(int chapter,int user_pattern)
+        {
+            Random random = new Random();
+            int looking = random.Next(0, 100);
+            if(looking<=20*chapter)
+            {
+                switch(looking)
+                {
+                    case 0:
+                        return 1;
+                    case 1:
+                        return 0;
+                    case 2:
+                        return 1;
+                    default:
+                        return 0;
+                }
+            }
+            else
+            {
+                return random.Next(0, 3);
+            }
+        }
+
+        private int BossOut(int value)
+        {
+            Random random = new Random();
+            return random.Next(0, value);
+        }
+
+    }
+}
